afb1c0d631fa84b85e5529826cf3376238bc757a
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2021-08-30 17:48:26 -0400

    test

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/cool$/;
+  console.log(request)
   // for (index in this.req.chunks){
   //   console.log(JSON.stringify(index))
   // }
