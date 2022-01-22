ee21998a223595ca353a2fc9774c5abab4905a5c
Adam Schoenfeld <adamrschoenfeld311@gmail.com>
2022-01-22 14:44:09 -0500

    Time to deploy

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const { Module } = require('module');
 
 var querystring = require("querystring")
 
-botID = "3964152c789e251f4a7d5c864b"
+botID = "bee00e962e7dd0d8fbc79f9f03"
 
 var queueDict = {
   "sunday": [],
